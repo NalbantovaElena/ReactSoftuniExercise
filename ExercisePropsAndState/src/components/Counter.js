c6ec72ada7707e0ee6d60fc1c26179c7e1f5d216
Elena Nalbantova <48248363+NalbantovaElena@users.noreply.github.com>
2022-04-29 13:50:27 +0300

    Create Counter.js
    Working with State whit class-based components

---
+++ ExercisePropsAndState/src/components/Counter.js
@@ -0,0 +1,23 @@
+import {Component} from 'react';
+
+class Counter extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            count:11
+        }
+    }
+
+    render(){
+        return(
+            <div className="counter">
+                <button onClick={(e)=>this.setState({count: this.state.count - 1})}>-</button>
+                <span>{this.state.count}</span>
+                <button onClick={(e)=>this.setState({count: this.state.count + 1})}>+</button>
+            </div>
+        )
+    }
+}
+
+export default Counter;
