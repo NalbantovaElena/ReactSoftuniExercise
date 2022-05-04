1eb8769510a22361dc7c5c05c9171cbc1f0e646e
Elena Nalbantova <48248363+NalbantovaElena@users.noreply.github.com>
2022-05-04 15:21:47 +0300

    Update Counter.js

--- ExercisePropsAndState/src/components/Counter.js
+++ ExercisePropsAndState/src/components/Counter.js
@@ -1,11 +1,12 @@
-import {Component} from 'react';
+import { Component } from 'react';
 
 class Counter extends Component{
 
     constructor(props){
         super(props);
         this.state = {
-            count:11
+            count:11,
+            collectionName: 'Petar'
         }
     }
 
@@ -13,11 +14,48 @@ class Counter extends Component{
         return(
             <div className="counter">
                 <button onClick={(e)=>this.setState({count: this.state.count - 1})}>-</button>
+               
+                //със setState променяме state-a,презаписва се само този state, който сме казали да се презапише,
+                // в случая count, не подаваме collectionName,  ако искаме и той да се презапише(защото не изчезва, както в JS)
+               //трябва да подадем и него, т.е трябва да стане следното
+               {/* onClick={(e)=>this.setState({count: this.state.count - 1},collectionName:'Elena')} */}
+               //ако не се подаде, не се променя стойността ,остава си същата ,без да се припокрива и не се изтрива.
+                <button onClick={(e)=>this.setState((oldState)=>({count: oldState.count - 1}))}>-</button>
+                //Т.е на setState подаваме функция ,която променя стойността на стария State, a не на this.state
+                //Защото е асинхронна функция
+               
                 <span>{this.state.count}</span>
                 <button onClick={(e)=>this.setState({count: this.state.count + 1})}>+</button>
+                //в onClick можем да зададем функция, която да се изпълнява при всеки click
             </div>
         )
     }
 }
 
 export default Counter;
+
+
+
+// import {Component} from 'react';
+
+// class Counter extends Component{
+
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//             count:11
+//         }
+//     }
+
+//     render(){
+//         return(
+//             <div className="counter">
+//                 <button onClick={(e)=>this.setState({count: this.state.count - 1})}>-</button>
+//                 <span>{this.state.count}</span>
+//                 <button onClick={(e)=>this.setState({count: this.state.count + 1})}>+</button>
+//             </div>
+//         )
+//     }
+// }
+
+// export default Counter;
