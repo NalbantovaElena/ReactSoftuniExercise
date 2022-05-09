8232e2385e6bd478761bae32578413bea0bdbc09
Elena Nalbantova <48248363+NalbantovaElena@users.noreply.github.com>
2022-05-09 13:33:08 +0300

    Update App.js
    exercise keys

--- reactComponentsDeepDive/App.js
+++ reactComponentsDeepDive/App.js
@@ -1,18 +1,14 @@
-import './App.css';
 
+import TodoList from './components/TodoList';
 
 function App() {
-  let numbers = [1,2,3,4,5,6]
-  // let mappedNumbers = numbers.map(number=><li>{number}</li>)
+ 
   return (
-    <div className="App">
-      <ul>
-        {numbers.map(number=><li>{number}</li>)}
-         {/* {mappedNumbers} */}
-      </ul>
-   
-    </div>
+ <div className='site-wrapper'>
+  <TodoList />
+</div>
   );
 }
 
 export default App;
+
