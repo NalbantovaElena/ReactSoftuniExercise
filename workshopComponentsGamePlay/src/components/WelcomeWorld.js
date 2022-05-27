99619a8167267c7b24b60e307d371160cbcda1ed
Elena Nalbantova <48248363+NalbantovaElena@users.noreply.github.com>
2022-05-27 15:49:21 +0300

    Update WelcomeWorld.js

--- workshopComponentsGamePlay/src/components/WelcomeWorld.js
+++ workshopComponentsGamePlay/src/components/WelcomeWorld.js
@@ -1,4 +1,15 @@
+import { useState,useEffect } from "react";
+import *as ServiceGame from  '../services/ServiceGame';
+import LatestGameCard from "./LatestGameCard";
 const WelcomeWorld = ()=>{
+    const [games,setGames] = useState([])
+    useEffect(()=>{
+ServiceGame.getLatest()
+.then(result=>{
+    setGames(result)
+})
+},[])
+
 return(
     <section id="welcome-world">
 
@@ -10,47 +21,14 @@ return(
 
     <div id="home-page">
         <h1>Latest Games</h1>
+{
+    
+    games.length>0
+    ?games.map(x => <LatestGameCard game={x}/>)
+    : <p className="no-articles">No games yet</p>
 
-      
-        <div className="game">
-            <div className="image-wrap">
-                <img src="./images/CoverFire.png" />
-            </div>
-            <h3>Cover Fire</h3>
-            <div className="rating">
-                <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-            </div>
-            <div className="data-buttons">
-                <a href="#" className="btn details-btn">Details</a>
-            </div>
-        </div>
-        <div className="game">
-            <div className="image-wrap">
-                <img src="./images/ZombieLang.png" />
-            </div>
-            <h3>Zombie Lang</h3>
-            <div className="rating">
-                <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-            </div>
-            <div className="data-buttons">
-                <a href="#" className="btn details-btn">Details</a>
-            </div>
-        </div>
-        <div className="game">
-            <div className="image-wrap">
-                <img src="./images/MineCraft.png" />
-            </div>
-            <h3>MineCraft</h3>
-            <div className="rating">
-                <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-            </div>
-            <div className="data-buttons">
-                <a href="#" className="btn details-btn">Details</a>
-            </div>
-        </div>
-
-      
-        <p className="no-articles">No games yet</p>
+}
+       
     </div>
 </section>
 )
