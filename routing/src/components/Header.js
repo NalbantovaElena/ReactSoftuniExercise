e18472aa0270d736d78e0138ff1a454519c6ca68
Elena Nalbantova <48248363+NalbantovaElena@users.noreply.github.com>
2022-05-28 15:44:01 +0300

    Update Header.js

--- routing/src/components/Header.js
+++ routing/src/components/Header.js
@@ -2,7 +2,7 @@ import {Link} from 'react-router-dom';
 const Header = () =>{
 return(
     <header>
-        <h1><Link className="home" to="/home">GamesPlay</Link></h1>
+        <h1><Link className="home" to="/">GamesPlay</Link></h1>
     <nav>
         <Link to="/games">All games</Link>
       
