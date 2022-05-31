3a19a14a5281356588dcb7f85a2764bb16e86a32
Elena Nalbantova <48248363+NalbantovaElena@users.noreply.github.com>
2022-05-31 13:57:08 +0300

    Update Header.js
    Add authenticated

--- workshopSPA/src/components/Header/Header.js
+++ workshopSPA/src/components/Header/Header.js
@@ -1,24 +1,36 @@
 import {Link} from 'react-router-dom'
-const Header = ()=>{
+const Header = ({
+    isAuthenticated,
+    username,
+})=>{
+    let guestNavigation = (
+        <div id="guest">
+           <Link className="button" to="/login">Login</Link>
+           <Link className="button" to="/register">Register</Link>
+        </div>
+        )
+    let userNavigation = (
+        <div id="user">
+        <span>Welcome, email</span>
+        <Link className="button" to="/my-pets">My Pets</Link>
+        <Link className="button" to="/create">Add Pet</Link>
+        <Link className="button" to="#">Logout</Link>
+    </div>
+    )    
     return(
-     
+    
         <header id="site-header">
        
         <nav className="navbar">
             <section className="navbar-dashboard">
                 <Link to="/">Dashboard</Link>
                 
-                <div id="guest">
-                    <Link className="button" to="/login">Login</Link>
-                    <Link className="button" to="/register">Register</Link>
-                </div>
+               {isAuthenticated
+               ?userNavigation
+               :guestNavigation
+               }
                
-                <div id="user">
-                    <span>Welcome, email</span>
-                    <Link className="button" to="/my-pets">My Pets</Link>
-                    <Link className="button" to="/create">Add Pet</Link>
-                    <Link className="button" to="#">Logout</Link>
-                </div>
+              
             </section>
         </nav>
     </header>
