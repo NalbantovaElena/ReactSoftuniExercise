29ccaefccc00667589fa025cc2a16d97c4396811
Elena Nalbantova <48248363+NalbantovaElena@users.noreply.github.com>
2022-05-31 13:55:24 +0300

    Update App.js
    Add Authenticated

--- workshopSPA/src/App.js
+++ workshopSPA/src/App.js
@@ -1,4 +1,6 @@
-import {Routes,Route} from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import {Routes,Route} from 'react-router-dom';
+import *as authService from './service/authService';
 import Dashboard from './components/Dashboard/Dashboard';
 import Header from './components/Header/Header'
 import MyPets from './components/MyPets/MyPets';
@@ -7,13 +9,24 @@ import Create from './components/Create/Create';
 import Register from './components/Register/Register';
 
 function App() {
+  const [userInfo,setUserInfo]= useState({isAuthenticated:false, username: ''})
+  useEffect(()=>{
+let user = authService.getUser();
+
+  },[])
+  const onLogin = (username)=>{
+    setUserInfo({
+      isAuthenticated:true,
+      user:username,
+    })
+  }
   return (
     <div id="container">
-  <Header />
+  <Header {...userInfo} />
 <main id="site-content">
   <Routes>
 <Route path="/" element={<Dashboard />} />
-<Route path="/login" element={<Login />} />
+<Route path="/login" element={<Login onLogin={onLogin}/>} />
 <Route path="/register" element={<Register />} />
 <Route path="/my-pets" element={<MyPets />} />
 <Route path="/create" element={<Create />} />
