20b701615b2906b61bb1ef6351eaf6c848671524
Elena Nalbantova <48248363+NalbantovaElena@users.noreply.github.com>
2022-06-23 13:14:06 +0300

    Update CharacterList.js
    custom hooks

--- ReactHooks/CharacterList.js
+++ ReactHooks/CharacterList.js
@@ -1,22 +1,10 @@
-import { useEffect, useState } from "react";
-
+import useFetch from "./hooks/useFetch";
 const CharacterList = ()=>{
-    const [characters, setCharacters] = useState([]);
-    useEffect(()=>{
-fetch('https://swapi.dev/api/people')
-.then(res=>res.json())
-.then(response=>{
-    //console.log(result);
-    setCharacters(response.results)
-});
-    },[]);
-
+    const characters = useFetch('https://swapi.dev/api/people')
 return(
     <ul>
 {characters.map(x => <li key = {x.name}>{x.name}</li>)}
     </ul>
-
 );
-};
-
+}
 export default CharacterList;
