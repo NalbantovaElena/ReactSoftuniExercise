4c09319ef6f319d46401b6e79c23ee662cd9e91b
Elena Nalbantova <48248363+NalbantovaElena@users.noreply.github.com>
2022-06-23 09:06:44 +0300

    Update app.js
    component did mount
    component did update

--- ReactHooks/app.js
+++ ReactHooks/app.js
@@ -7,7 +7,7 @@ import './App.css';
 //Hooks Changed
 function App() {
  //state- a държи някакъв низ. Това връща масив.
-  const [stateValue, setStateValue] = useState(''); //stateValue - стойността на стринга, setStateValue e ф-ция
+  const [stateValue, setStateValue] = useState('Pesho'); //stateValue - стойността на стринга, setStateValue e ф-ция
   //console.log(stateValue,setStateValue)
 
   //за да изпълним фунцията setStateValue ще използваме useEffect, да променим стринга в движение
@@ -18,20 +18,48 @@ function App() {
     age:'30',
     hobbies:['first','second','third']
   })
-  useEffect(()=>{ // с useEffect app-a се рендерира веднъж
+    //component didUpdate
+    useEffect(()=>{ // с useEffect app-a се рендерира веднъж
       setTimeout(()=>{ //след това изпълнява ф-цията setTimeout с която се сменя state-a 
   //и понеже хуковете работят така че при промяна на даден state , отново компонента се пререндерира
-setStateValue('Pesho');
+setStateValue(oldState=>oldState+'o');//при всяка промяна на count към стойността на state-a се добавя 'о'
 setInfo(oldState=>({
   ...oldState,
   age:31,
+ hobbies: [...oldState.hobbies,'forty']
+
 }))
+
 },1500)
-  },[])
+  },[count])//при промяна на count пререндерира
+//   //component didMount
+//   useEffect(()=>{ // с useEffect app-a се рендерира веднъж
+//       setTimeout(()=>{ //след това изпълнява ф-цията setTimeout с която се сменя state-a 
+//   //и понеже хуковете работят така че при промяна на даден state , отново компонента се пререндерира
+// setStateValue('Pesho');
+// setInfo(oldState=>({
+//   ...oldState,
+//   age:31,
+//  hobbies: [...oldState.hobbies,'forty']
+
+// }))
+
+// },1500)
+//   },[])//с празния масив компонента се пререндерира веднъж 
+
+  const hobbi = []
+  info.hobbies.forEach((data) => {
+    hobbi.push(<h2>{data}</h2>)
+    console.log(hobbi)
+  })
+
   return (
+    
     <div className="App">
      <h1>{!stateValue ? 'Loading...': stateValue}</h1>
-     <h2>{info.age}</h2>
+     {/* <h2>{info.age}</h2> */}
+    
+     <h2>{hobbi}</h2>
      <h3>{count}</h3>
      <button onClick = {()=>setCount(x=>x+1)}>+</button>
     </div>
